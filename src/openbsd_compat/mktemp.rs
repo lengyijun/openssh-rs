5e2dc1d6d090c5125d8e92a8ed95f9b8f490a023
Your Name <you@example.com>
2023-05-24 15:54:11 +0800

    mkstemp

--- src/openbsd_compat/mktemp.rs
+++ src/openbsd_compat/mktemp.rs
@@ -1,8 +1,5 @@
 use ::libc;
-extern "C" {
-    
-    fn mkstemp(_: *mut libc::c_char) -> libc::c_int;
-}
+
 pub type __mode_t = libc::c_uint;
 pub type mode_t = __mode_t;
 #[no_mangle]
@@ -10,7 +7,7 @@ pub unsafe extern "C" fn _ssh_mkstemp(mut template: *mut libc::c_char) -> libc::
     let mut mask: mode_t = 0;
     let mut ret: libc::c_int = 0;
     mask = libc::umask(0o177 as libc::c_int as __mode_t);
-    ret = mkstemp(template);
+    ret = libc::mkstemp(template);
     libc::umask(mask);
     return ret;
 }
